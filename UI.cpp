49a9fccc39f7d027c2a9eb291d9452704dc6662b
sherif <s-sherif.awny@zewialcity.edu.eg>
2024-01-07 07:35:49 +0200

    ui class

--- UI.cpp
+++ UI.cpp
@@ -1,11 +1,58 @@
 #include "UI.h"
-using namespace std;
 
-UI::UI()  
-{
-    
+#include <iostream>
+#include <conio.h>
+#include <chrono>
+#include <thread>
+
+// Class implementation for UI
+UI::UI() {
+    ApplicationMode = Mode::Interactive;
+    setMode();
 }
 
+void UI::setMode() {
+    int modeNum = -1;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "Please Choose Your Interface Mode: \n [1] For Interactive Mode \n [2] For Silent Mode" << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cin >> modeNum;
+    while (modeNum > 2 || modeNum < 1) {
+        std::cout << "Please Enter A Valid Number: " << std::endl;
+        std::cin >> modeNum;
+    }
+    switch (modeNum) {
+        case 1:
+            ApplicationMode = Mode::Interactive;
+            break;
+        case 2:
+            ApplicationMode = Mode::Silent;
+            break;
+        default:
+            ApplicationMode = Mode::Silent;
+            break;
+    }
+}
 
+Mode UI::GetCurrentMode() const {
+    return ApplicationMode;
+}
 
+void UI::printMessage(std::string Message) const {
+    if (ApplicationMode == Mode::Interactive) {
+        IntercativeModeOutput(Message);
+    }
+    if (ApplicationMode == Mode::Silent) {
+        StepByStepModeOutput(Message);
+    }
+}
 
+void UI::IntercativeModeOutput(std::string Message) const {
+    std::cout << Message << std::endl;
+    _getch();
+}
+
+void UI::StepByStepModeOutput(std::string Message) const {
+    std::cout << Message << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
--- UI.h
+++ UI.h
@@ -1,26 +1,44 @@
+#pragma once
 
+#include <string>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include "ArrayQueue.h"
-#include "Event.h"
-#include "ArrivalEvent.h"
-#include "LeaveEvent.h"
-#include "Passenger.h"
-#include "Station.h"
-#include "SimulationTime.h"
-using namespace std;
 
-class UI 
+// Enumerations for different event and passenger types
+enum Event_Type
 {
-public:
-
-    UI();
-
-   
-
-private:
-
+    Arrival,
+    leave,
     
 };
 
+enum Passenger_Type {
+    NP,	// Normal Passenger Type
+    SP,	// Special Passenger Type
+    wp	// Wheel_chair Passenger Type
+};
+
+enum Bus_Type {
+    MB,	// Mixed Bus
+    WB,	// Wheel Bus
+   
+};
+
+enum Mode {
+    Interactive,
+    Silent,
+  
+};
+
+// Class representing the UI
+class UI {
+private:
+    Mode ApplicationMode;
+    
+public:
+    UI();
+    void setMode();
+    Mode GetCurrentMode() const;
+    void printMessage(std::string message) const;
+    void IntercativeModeOutput(std::string message) const;
+    void StepByStepModeOutput(std::string message) const;
+};
